623a378b0c90262ce7d86e37b24ff5c65e2ed372
Meet Patel <meetp4499@gmail.com>
2026-03-17 21:17:11 +0530

    build: add CMake build system with SQLite3 integration

---
+++ build/CMakeFiles/bank.dir/link.d
@@ -0,0 +1,124 @@
+bank: \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crt1.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crti.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/crtbegin.o \
+  CMakeFiles/bank.dir/src/main.cpp.o \
+  CMakeFiles/bank.dir/src/AccountManager.cpp.o \
+  CMakeFiles/bank.dir/src/BankAccount.cpp.o \
+  CMakeFiles/bank.dir/src/DatabaseManager.cpp.o \
+  CMakeFiles/bank.dir/src/InputValidator.cpp.o \
+  CMakeFiles/bank.dir/src/LoanManager.cpp.o \
+  CMakeFiles/bank.dir/src/Logger.cpp.o \
+  CMakeFiles/bank.dir/src/RDManager.cpp.o \
+  CMakeFiles/bank.dir/src/StandingInstructionManager.cpp.o \
+  CMakeFiles/bank.dir/src/Utils.cpp.o \
+  /usr/lib64/libsqlite3.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libstdc++.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so \
+  /lib64/libm.so.6 \
+  /lib64/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so \
+  /lib64/libc.so.6 \
+  /usr/lib64/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/15/crtend.o \
+  /usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crtn.o \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crt1.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crti.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/crtbegin.o:
+
+CMakeFiles/bank.dir/src/main.cpp.o:
+
+CMakeFiles/bank.dir/src/AccountManager.cpp.o:
+
+CMakeFiles/bank.dir/src/BankAccount.cpp.o:
+
+CMakeFiles/bank.dir/src/DatabaseManager.cpp.o:
+
+CMakeFiles/bank.dir/src/InputValidator.cpp.o:
+
+CMakeFiles/bank.dir/src/LoanManager.cpp.o:
+
+CMakeFiles/bank.dir/src/Logger.cpp.o:
+
+CMakeFiles/bank.dir/src/RDManager.cpp.o:
+
+CMakeFiles/bank.dir/src/StandingInstructionManager.cpp.o:
+
+CMakeFiles/bank.dir/src/Utils.cpp.o:
+
+/usr/lib64/libsqlite3.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libstdc++.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libm.so:
+
+/lib64/libm.so.6:
+
+/lib64/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/libc.so:
+
+/lib64/libc.so.6:
+
+/usr/lib64/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/crtend.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/15/../../../../lib64/crtn.o:
+
+/lib64/ld-linux-x86-64.so.2:
