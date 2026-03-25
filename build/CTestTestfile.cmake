914b1745a55adc28aaecbbb47951fdb477122c2d
Meet Patel <meetp4499@gmail.com>
2026-03-25 15:34:57 +0530

    chore: add .gitignore, remove build artifacts and editor files

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/meetpatel12_/Stuff/Code/C++/Bank Management system
-# Build directory: /home/meetpatel12_/Stuff/Code/C++/Bank Management system/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/meetpatel12_/Stuff/Code/C++/Bank Management system/build/bank_tests[1]_include.cmake")
